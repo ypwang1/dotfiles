653a9ed7eb1ce7a1b6dffdfcc2f66fb831360a92
Yupeng Wang <yupengori@gmail.com>
2025-10-21 15:29:23 +0200

    Add safe Devpod install script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
-# Define a function which rename a `target` file to `target.backup` if the file
-# exists and if it's a 'real' file, ie not a symlink
 backup() {
   target=$1
   if [ -e "$target" ]; then
     if [ ! -L "$target" ]; then
       mv "$target" "$target.backup"
       echo "-----> Moved your old $target config file to $target.backup"
+    else
+      rm "$target"
     fi
   fi
 }
@@ -15,61 +15,48 @@ backup() {
 symlink() {
   file=$1
   link=$2
-  if [ ! -e "$link" ]; then
-    echo "-----> Symlinking your new $link"
-    ln -s $file $link
-  fi
+  ln -sf "$file" "$link"
+  echo "-----> Symlinked $file -> $link"
 }
 
-# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
-# and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
+# Dotfiles
 for name in aliases gitconfig irbrc pryrc rspec zprofile zshrc; do
-  if [ ! -d "$name" ]; then
-    target="$HOME/.$name"
-    backup $target
-    symlink $PWD/$name $target
-  fi
+  target="$HOME/.$name"
+  backup $target
+  symlink "$PWD/$name" $target
 done
 
-# Install zsh-syntax-highlighting plugin
-CURRENT_DIR=`pwd`
+# Zsh plugins
 ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting
-fi
-cd "$CURRENT_DIR"
+mkdir -p "$ZSH_PLUGINS_DIR"
+cd "$ZSH_PLUGINS_DIR"
+[ ! -d zsh-autosuggestions ] && git clone https://github.com/zsh-users/zsh-autosuggestions
+[ ! -d zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting
+cd "$PWD"
 
-# Symlink VS Code settings and keybindings to the present `settings.json` and `keybindings.json` files
-# If it's a macOS
+# VSCode settings
 if [[ `uname` =~ "Darwin" ]]; then
   CODE_PATH=~/Library/Application\ Support/Code/User
-# Else, it's a Linux
 else
   CODE_PATH=~/.config/Code/User
-  # If this folder doesn't exist, it's a WSL
-  if [ ! -e $CODE_PATH ]; then
-    CODE_PATH=~/.vscode-server/data/Machine
-  fi
+  [ ! -e $CODE_PATH ] && CODE_PATH=~/.vscode-server/data/Machine
 fi
+mkdir -p "$CODE_PATH"
 
 for name in settings.json keybindings.json; do
   target="$CODE_PATH/$name"
   backup $target
-  symlink $PWD/$name $target
+  symlink "$PWD/$name" $target
 done
 
-# Symlink SSH config file to the present `config` file for macOS and add SSH passphrase to the keychain
+# SSH config (macOS only)
 if [[ `uname` =~ "Darwin" ]]; then
   target=~/.ssh/config
   backup $target
-  symlink $PWD/config $target
+  symlink "$PWD/config" $target
   ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 fi
 
-# Refresh the current terminal with the newly installed configuration
 exec zsh
+echo "👌 Dotfiles setup complete!"
 
-echo "👌 Carry on with git setup!"
